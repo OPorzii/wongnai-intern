5190613ccb9188966520d7b378581be66c9d3456
Unknown <59070103@it.kmitl.ac.th>
2019-04-15 14:58:42 +0700

    Complete assignment : Step 3
    - Edit Query for Case-Insensitive in MovieRepository
    - Change Bean in Controller
    - Implement method forceSync() for download json and save in-memory database when start web server

--- src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
+++ src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
@@ -2,11 +2,16 @@ package com.wongnai.interview.movie.sync;
 
 import javax.transaction.Transactional;
 
+import com.wongnai.interview.movie.Movie;
+import com.wongnai.interview.movie.external.MoviesResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.wongnai.interview.movie.MovieRepository;
 import com.wongnai.interview.movie.external.MovieDataService;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class MovieDataSynchronizer {
@@ -19,5 +24,17 @@ public class MovieDataSynchronizer {
 	@Transactional
 	public void forceSync() {
 		//TODO: implement this to sync movie into repository
+
+		MoviesResponse moviesResponse = movieDataService.fetchAll();
+
+		//Convert Object MovieData to Movie
+		List<Movie> result = moviesResponse.stream()
+				.map(movie -> {
+					Movie obj = new Movie();
+					obj.setName(movie.getTitle());
+					obj.setActors(movie.getCast());
+					return obj;
+				}).collect(Collectors.toList());
+		movieRepository.saveAll(result);
 	}
 }
